f4335dda087080b3473597a503eed65006126941
Each9084 <each9084@gmail.com>
2025-12-07 21:41:01 +0800

    draw the basic ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,69 @@
+import 'dart:io';
+
+import 'package:each_reader/core/services/BookLibraryService.dart';
+import 'package:each_reader/core/themes/theme_service.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const EachReaderApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class EachReaderApp extends StatelessWidget {
+  const EachReaderApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return MultiProvider(providers: [
+      // 1. 注入书籍管理服务
+      ChangeNotifierProvider(create: (context) {
+        //create是必须的,这样才能通过ChangeNotifierProvider管理实例的的生命周期
+        final service = Booklibraryservice();
+        service.loadInitialBook(); // 在服务创建时加载一些测试数据
+        return service;
+      },),
+      // 2. 注入主题管理服务 我们不需要其他参数所以采用"_"
+      ChangeNotifierProvider(create: (_) => ThemeService()),
+    ],
+        //使用 Consumer<ThemeService> 的目的是为了监听 (Listen) ThemeService 的变化
+        child:
+            //themeService继承自 ChangeNotifier，当用户点击“白天模式”或“夜晚模式”时，它内部会调用 notifyListeners()
+            // Consumer<ThemeService> 的作用就像一个“收音机”，它专门收听来自 ThemeService 的广播。
+        //一旦收到广播（即 notifyListeners() 被调用），Consumer 就会执行它的 builder 方法，重新构建其内部的 Widget。
+        Consumer<ThemeService>(
+          builder: (context, themeService, child) {
+            return MaterialApp(
+              title: "Each Reader",
+              debugShowCheckedModeBanner: false,
+              theme: themeService.themeWithFont,
+              home: _PlatformEntryPoint(), // 跳转到平台入口判断
+            );
+          },
+
+        )
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+// 平台入口判断
+class _PlatformEntryPoint extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: .center,
-          children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
+    if (kIsWeb) {
+      // Web 平台假设使用 Windows 风格布局
+      return WindowsMainScreen();
+    } else if (Platform.isWindows) {
+      return WindowsMainScreen();
+    } else if (Platform.isAndroid) {
+      return AndroidMainScreen();
+    } else {
+      return Scaffold(
+        body: Center(child: Text("Unsupported Platform"),),
+      )
+    }
   }
+
 }
+
+
